592fc8ecbbd8a1ae9494bbad797c25c058a7b949
LeahK <duello.1@osu.edu>
2016-04-23 22:58:18 -0400

    Fixed minor issue with the login activity title and the application title.

--- login_screen/src/main/java/com/example/leah/myapplication/LoginActivity.java
+++ login_screen/src/main/java/com/example/leah/myapplication/LoginActivity.java
@@ -78,6 +78,10 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        // set title
+        this.setTitle("Login");
+
         // Set up the login form.
         mEmailView = (AutoCompleteTextView) findViewById(R.id.email);
         populateAutoComplete();
